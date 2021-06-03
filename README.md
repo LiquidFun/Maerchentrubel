7e388823bdcc2d6d2103a9443a76eac8eb2d5658
LiquidFun <brutenis@gmail.com>
2021-06-04 00:41:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NØRDJAM - Pixelords
+
+
+
+# Attributions (currently in use, may change)
+
+
+Font: [ChiKareGo2 by Giles Booth](http://www.pentacom.jp/pentacom/bitfontmaker2/gallery/?id=3780) (License: Creative Commons Attribution)
+Music: [Heartache by Jim Hall](https://freemusicarchive.org/music/jim-hall/synth-kid-elsewhere/heartache) (License: CC BY 4.0)
---
+++ Scripts/SoundManager.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+const MAX_VOLUME = 20
+const MIN_VOLUME = -50
+
+var music_on = true
+var volume_modifier = 0
+var music_player = null
+
+func play(file_path, volume=-30):
+	if music_on:
+		var audio_player = AudioStreamPlayer.new()
+		
+		self.add_child(audio_player)
+		audio_player.stream = load(file_path)
+		audio_player.volume_db = clamp(volume + volume_modifier, -80, 0)
+		audio_player.play()
+
+func toogle_music():
+	music_on = not music_on
+	
+	if not music_on:
+		music_player.volume_db = -80
+	else:
+		music_player.volume_db = clamp(-35 + volume_modifier, -80, 0)
+
+func change_volume(amount):
+	volume_modifier = clamp(volume_modifier + amount, MIN_VOLUME, MAX_VOLUME)
+	if music_on:
+		music_player.volume_db = clamp(-35 + volume_modifier, -80, 0)
+
+func _ready():
+	music_player = AudioStreamPlayer.new()
+		
+	self.add_child(music_player)
+	music_player.stream = load("res://Resources/Sound/Music/Jim Hall - Heartache.mp3")
+	music_player.volume_db = -35
+	music_player.play()
