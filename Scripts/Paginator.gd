d62a96ed6191204feaec88e13be8874267da09de
justinnk <17525874+justinnk@users.noreply.github.com>
2021-06-05 12:48:37 +0200

    Implement simple book and main menu.

---
+++ Scripts/Paginator.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var current_pos = 0
+var max_cycle = 4
+
+var cycle = {
+	1: [@"Page1", @"Page2"],
+	2: [@"Page3", @"Page4"],
+	3: [@"Page5", @"Page6"],
+	4: [@"Page7", @"Page8"]
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Next_button_up():
+	if current_pos >= max_cycle:
+		return
+	current_pos += 1
+	for p in cycle[current_pos]:
+		get_node(p).show()
+	pass # Replace with function body.
+
+
+func _on_Previous_button_up():
+	if current_pos <= 1:
+		return
+	for p in cycle[current_pos]:
+		get_node(p).hide()
+	current_pos -= 1
+	pass # Replace with function body.
