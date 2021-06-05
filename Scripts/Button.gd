fbf39adc70f22c57828715032261a102337e8b91
lg218 <lg218@uni-rostock.de>
2021-06-05 14:51:56 +0200

    added button and obstacle objects, added battle screen

---
+++ Scripts/Button.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_area_entered(area):
+	$AnimatedSprite.animation = "on"
+	
+func _on_Button_area_exited(area):
+	$AnimatedSprite.animation = "off"
